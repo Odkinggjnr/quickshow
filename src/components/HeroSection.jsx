fd54d7206877b926aaf9c84a35a9dbf12a3295a0
Odkinggjnr <chuksamitox@gmail.com>
2025-10-29 01:54:17 +0100

    updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {CalendarIcon, ClockIcon, ArrowRight} from 'lucide-react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
+import bgImage from '../assets/backgroundImage.png'
 import { useNavigate } from 'react-router-dom'
 const HeroSection = () => {
     const navigate = useNavigate();
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
+    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center h-screen'  style={{ backgroundImage: `url(${bgImage})` }}>
       
           <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20' />
           <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>Guardians <br />of the Galaxy</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
-
+import TrailersSection from '../components/TrailersSection'
 const Home = () => {
   return (
     <div>
       <HeroSection />
-      <FeaturedSection/>
+      <FeaturedSection />
+      <TrailersSection/>
     </div>
   )
 }
